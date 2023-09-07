d92c14385840ed340a13cec253790474b0ca09c2
Teacher <informatikvbg@gmail.com>
2023-09-07 17:53:27 +0300

    2

--- 3.js
+++ 3.js
@@ -1,27 +1,24 @@
 let proverka = document.querySelector("a");
 let dokazat = document.querySelector("input");
 
+proverka.className = "button";
+dokazat.className = "first";
 // proverka.onclick = showTags;
 // dokazat.onclick = showTags;
 
-function showTags(event) {
-  const tags = event.currentTarget.tagName;
-  alert("tags: " + tags);
+function showTags() {
+  this.classList.remove("first");
+  this.classList.toggle("second");
+  console.log(this.classList);
 }
 
-// dokazat.addEventListener("click", showTags);
-// proverka.addEventListener("click", showTags);
+dokazat.addEventListener("click", showTags);
+proverka.addEventListener("click", showTags);
 // if (proverka) {
 //     proverka.removeEventListener("click", showTags);}
 
-dokazat.className = "first";
-dokazat.addEventListener("click", function (event) {
-    
-    dokazat.classList.remove("first");
-    dokazat.classList.toggle("second");
-    console.log(dokazat.classList);
-});
-dokazat.addEventListener("transitioned", function (event) {
-    
-    dokazat.classList.remove("first");
-    dokazat.classList.toggle("second");})
+// dokazat.addEventListener("click", function (event) {
+//   dokazat.classList.toggle("second");
+//   console.log(dokazat.classList);
+// });
+dokazat.addEventListener("transitionend", showTags);
